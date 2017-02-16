e777b8c94dcdfa6ffb76110b71ea5fa22ffb23f3
Aditya Prasad <aytida77@gmail.com>
2017-02-17 00:46:38 +0530

    bug fix for gen_go

--- gen_go.sh
+++ gen_go.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-protoc --proto_path=src --go_out=$PROTO_GO_OUT_DIR common.proto organization.proto
+protoc --proto_path=./ --go_out=$PROTO_GO_OUT_DIR common.proto organization.proto
